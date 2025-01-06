4be212d4193548df0311ecae9d28dbbd4f3d1e5e
KIPOKA <ladykipoka@gmail.com>
2025-01-06 10:54:24 +0200

    added commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -28,60 +28,41 @@ const Contact = () => {
   };
 
   return (
-    <section className="bg-dark_primary text-white py-14" id="contact">
+    <section
+      className="bg-gradient-to-r from-gray-800 to-gray-600 text-white py-14"
+      id="contact"
+    >
       <Toaster />
       <div className="container mx-auto px-5">
         <div className="text-center mb-10">
           <h2
-            className="text-3xl md:text-4xl font-semibold text-white"
+            className="text-3xl md:text-4xl font-semibold"
             data-aos="fade-down"
           >
             {Contact.title}
           </h2>
           <h4
-            className="text-lg md:text-xl text-gray-300 mt-3 text-white"
+            className="text-lg md:text-xl text-gray-300 mt-3"
             data-aos="fade-down"
           >
             {Contact.subtitle}
           </h4>
         </div>
 
-        <div className="flex flex-col md:flex-row gap-2 justify-between items-start">
-          {/* Social Media Section on the left */}
-          <div className="flex flex-col gap-6 w-full md:w-1/3">
-            {Contact.social_media.map((content, i) => (
-              <div
-                key={i}
-                data-aos="fade-down"
-                data-aos-delay={i * 200}
-                className="flex items-center gap-3 text-lg"
-              >
-                <h4 className="text-white">{createElement(content.icon)}</h4>
-                <a
-                  className="text-white hover:underline"
-                  href={content.link}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  {content.text}
-                </a>
-              </div>
-            ))}
-          </div>
-
+        <div className="flex flex-col md:flex-row gap-6 justify-center items-center">
           {/* Form Section on the right */}
           <form
             ref={form}
             onSubmit={sendEmail}
             data-aos="fade-up"
-            className="bg-white p-8 rounded-lg shadow-xl flex flex-col gap-2 w-1/2  "
+            className="bg-[#1c7ed6] p-8 rounded-lg shadow-xl flex flex-col gap-4 w-full md:w-1/2"
           >
             <input
               type="text"
               name="from_name"
               placeholder="Your Name"
               required
-              className="border border-slate-600 p-3 rounded-md focus:ring-2 focus:ring-green-600 focus:outline-none text-black"
+              className="border border-gray-300 p-3 rounded-md focus:ring-2 focus:ring-green-600 focus:outline-none text-black transition duration-300 hover:border-green-500"
             />
             <input
               type="email"
@@ -89,23 +70,51 @@ const Contact = () => {
               pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}"
               placeholder="Your Email"
               required
-              className="border border-slate-600 p-3 rounded-md focus:ring-2 focus:ring-green-600 focus:outline-none text-black"
+              className="border border-gray-300 p-3 rounded-md focus:ring-2 focus:ring-green-600 focus:outline-none text-black transition duration-300 hover:border-green-500"
             />
-
             <textarea
               name="message"
               placeholder="Your Message"
               required
-              className="border border-slate-600 p-3 rounded-md h-44 focus:ring-2 focus:ring-green-600 focus:outline-none text-black"
+              className="border border-gray-300 p-3 rounded-md h- 44 focus:ring-2 focus:ring-green-600 focus:outline-none text-black transition duration-300 hover:border-green-500"
             ></textarea>
-            <button
-              type="submit"
-              className="btn bg-green-600 text-white py-3 rounded-md hover:bg-green-700 transition duration-300"
-            >
-              Submit
-            </button>
+            <div className="flex justify-center">
+              <button
+                type="submit"
+                className="bg-green-500 text-white py-3 rounded-md hover:bg-green-600 transition duration-300 w-[40%] flex items-center justify-center"
+              >
+                Submit
+              </button>
+            </div>
           </form>
         </div>
+      </div>
+      {/* Social Media Section on the left */}
+
+      <div className="flex flex-col md:flex-row gap-6 w-full md:w-1/3 items-center justify-center text-black">
+        {Contact.social_media.map((content, i) => (
+          <div
+            key={i}
+            data-aos="fade-down"
+            data-aos-delay={i * 200}
+            className="flex flex-col items-center md:flex-row gap-3 text-lg"
+          >
+            {/* Icon */}
+            <div className="p-3 bg-gray-200 rounded-full flex items-center justify-center">
+              {createElement(content.icon, { className: "text-xl text-black" })}
+            </div>
+
+            {/* Text and Link */}
+            <a
+              href={content.link}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-black hover:underline"
+            >
+              {content.text}
+            </a>
+          </div>
+        ))}
       </div>
     </section>
   );
